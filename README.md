8625bc051a29528c888e38d616556770c5741c50
Liz Chhouk <kisaras.playground@gmail.com>
2017-04-29 23:07:34 -0400

    handles errors now

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Getting Started
-This is a quick little side project that takes advantage of the Zillow API.
+This is a quick little side project that takes advantage of the Zillow API. Apparently the Zillow API doesn't play nice with JavaScript (???), so this is just a Ruby/Sinatra app.
 
-You'll need to run the following commands to get started!
+Run the following commands to get started.
 
 `bundle install`
 `ruby server.rb`
--- server.rb
+++ server.rb
@@ -5,44 +5,93 @@ require 'pry'
 require 'crack'
 
 get '/' do
+  @params = {
+    "address" => "",
+    "city" => "",
+    "state" => "",
+    "zip" => ""
+  }
+  @param_errors = nil
   erb :index
 end
 
 post '/calculator' do
-  query = get_query(request.body.read)
+  my_response = request.body.read
+  @params = get_params(my_response)
+  @param_errors = validate_params(@params)
 
-  uri = URI("http://www.zillow.com/webservice/GetSearchResults.htm"+query)
-  res = Net::HTTP.get_response(uri)
-  data = Crack::XML.parse(res.body)
+  if @param_errors
+    erb :index
+  else
+    query = get_query(@params)
 
-  @zillowData = data["SearchResults:searchresults"]["response"]["results"]["result"]
-  erb :results
+    uri = URI("http://www.zillow.com/webservice/GetSearchResults.htm"+query)
+    res = Net::HTTP.get_response(uri)
+    data = Crack::XML.parse(res.body)
+
+    if data["SearchResults:searchresults"]["message"]["text"].include?("Error")
+      @error = "There were no results for that address."
+      @zillowData = nil
+    else
+      @error = nil
+      @zillowData = data["SearchResults:searchresults"]["response"]["results"]["result"]
+    end
+    erb :results
+  end
 end
 
-def get_query (data)
+def get_params(data)
   data_array = data.split("&")
-
   my_params = Hash.new
 
   data_array.each do |element|
     element_array = element.split("=")
     key = element_array[0]
-    value = element_array[1]? element_array[1] : ""
+    value = element_array[1] ? element_array[1] : ""
     my_params[key] = value
   end
 
-  address = my_params["address"]
-  city = my_params["city"]
-  state = my_params["state"]
-  zip = my_params["zip"]
+  return my_params
+end
+
+def validate_params(params)
+  errors = nil
+
+  proto_errors = []
+
+  if params["address"] === ""
+    proto_errors.push "Address cannot be empty."
+  end
+
+  params["address"] = params["address"].gsub("+"," ")
+
+  if !/^\d{5}(-\d{4})?$/.match(params["zip"])
+    if params["city"] === "" || params["state"] === ""
+      proto_errors.push "Zip Code must be five digits."
+    end
+    if params["city"] === ""
+      proto_errors.push "City cannot be empty."
+    end
+    if params["state"] === ""
+      proto_errors.push "State cannot be empty."
+    end
+  end
+
+  if proto_errors.size > 0
+    errors = proto_errors
+  end
+
+  return errors
+end
+
+def get_query(params)
+  address = params["address"]
+  city = params["city"]
+  state = params["state"]
+  zip = params["zip"]
 
   addressQuery = address
-
-  cityStateZipQuery = if zip
-      zip
-    else
-      "#{city}%2+#{state}"
-    end
+  cityStateZipQuery = /^\d{5}(-\d{4})?$/.match(zip) ? zip : "#{city}%2C+#{state}"
 
   query = "?zws-id=X1-ZWz198oaiq7qq3_7x5fi&address=#{addressQuery}&citystatezip=#{cityStateZipQuery}"
 end
